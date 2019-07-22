66e8eca8902763c614bae43836eead2a77340bf3
cssi-nyc-2019 <53194522+cssi-nyc-2019@users.noreply.github.com>
2019-07-22 16:00:30 -0400

    Add files via upload

---
+++ main.py
@@ -0,0 +1,25 @@
+# the import section
+import webapp2
+import jinja2
+import os
+
+# this initializes the jinja2 environment
+# this will be the same in every app that uses the jinja2 templating library 
+the_jinja_env = jinja2.Environment(
+  loader=jinja2.FileSystemLoader(os.path.dirname(__file__)),
+  extensions=['jinja2.ext.autoescape'],
+  autoescape=True)
+
+# other functions should go above the handlers or in a separate file
+
+# the handler section
+class MainHandler(webapp2.RequestHandler):
+  def get(self):  # for a get request
+    self.response.write('Welcome')  # the response
+
+
+# the app configuration section	
+app = webapp2.WSGIApplication([
+  #('/', MainPage),
+  ('/', MainHandler),
+  ], debug=True)
